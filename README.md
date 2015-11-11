9b04bcfbe6879c9bf80e1cb3ce56c959a1cc3f42
Nithish <blu3b0lt@devil.(none)>
2015-11-11 15:36:07 +0530

    Configuration File

--- README.md
+++ README.md
@@ -1,10 +1,19 @@
+
 #**Notify-Quote:**
-This is a simple python script to fetch quotes from **forismatic** and display them using **notify-send**. Script is fairly simple.
+This is a simple python script to fetch quotes from **[forismatic](http://forismatic.com/)** and display them using **notify-send**. Script is fairly simple.
+
+API Documentation: http://forismatic.com/en/api/
 
 #Steps to run: 
 >1. Download the icon and notify-quote.py 
 >2. pip install -r requirements.txt - to install required dependencies 
 >3. python notify-quote.py
 
+#Configuration
+Two languages "English"  and "Russian' are supported, language can be configured using **lang** property in ***config.json*** to en or ru for English or Russian respectively.
+
 #TO DO:
 >1. Autostart Script
+
+
+
--- notify-quote.py
+++ notify-quote.py
@@ -11,13 +11,22 @@ import json
 import subprocess
 import time
 import os
+import sys
 
+currentDirectory = os.getcwd()
+configFilePath = currentDirectory + "/config.json"
+iconPath = currentDirectory + "/icon.png"
+if os.path.exists(configFilePath) != False:
+	configFile = open(configFilePath, "r")
+	configFileJson = json.loads(configFile.read())
+	lang = configFileJson['lang']
+	configFile.close()
+else:
+	lang = "en"
 while True:
-	response = requests.get("http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json")
+	response = requests.get("http://api.forismatic.com/api/1.0/?method=getQuote&lang="+lang+"&format=json")
 	responseJson = json.loads(response.text)
 	author = responseJson['quoteAuthor']
 	quote = responseJson['quoteText']
-	currentDirectory = os.getcwd()
-	iconPath = currentDirectory + "/icon.png"
 	subprocess.call(["notify-send", "-u", "normal", "-t", "10000", "-c", "Quotes", "-i", iconPath, author+" says" , '" '+quote+' "'])
 	time.sleep(2400)
